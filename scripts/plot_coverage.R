bb113b70c4676a63b91b09ae5d51a110ade86f60
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-11 15:52:39 -0500

    Update

--- scripts/plot_coverage.R
+++ scripts/plot_coverage.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 inputFile = "~/data/Bge/BGE.bga.bedgraph"
 
-all.data <- read.csv(inputFile, header = FALSE, sep = "\t")
+all.data <- read.csv(inputFile, header = FALSE, sep = "\t", quote = "")
 
 filtered.data <- all.data %>%
   mutate(C_weight = V4*(V3-V2+1))
@@ -13,7 +13,7 @@ summ.data <- filtered.data %>%
   summarise(C_ave = mean(C_weight), C_length = max(V3)) %>%
   arrange(desc(C_ave))
   
-
+write.table(summ.data, "~/data/Bge/summ_data.csv", sep = "\t")
 
 
 
