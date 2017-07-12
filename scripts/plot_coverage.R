57a7dc4dd29cf282ccbacf4bf9a5e23fe882f0fb
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-12 09:32:26 -0500

    Piped for efficient data handling

--- scripts/plot_coverage.R
+++ scripts/plot_coverage.R
@@ -5,15 +5,23 @@ inputFile = "~/data/Bge/BGE.bga.bedgraph"
 
 all.data <- fread(inputFile)
 
-filtered.data <- all.data %>%
-  mutate(C_weight = V4*(V3-V2+1))
+# filtered.data <- all.data %>%
+#   mutate(C_weight = V4*(V3-V2+1))
   
-summ.data <- filtered.data %>%  
+# summ.data <- filtered.data %>%  
+#   group_by(V1) %>%
+#   #mutate(C_length = max(V3)) %>%
+#   summarise(C_ave = mean(C_weight), C_length = max(V3)) %>%
+#   arrange(desc(C_ave))
+  
+summ.data <- all.data %>%
+  mutate(C_weight = V4*(V3-V2+1)) %>%
   group_by(V1) %>%
   #mutate(C_length = max(V3)) %>%
   summarise(C_ave = mean(C_weight), C_length = max(V3)) %>%
   arrange(desc(C_ave))
-  
+
+
 write.table(summ.data, "~/data/Bge/summ_data.csv", sep = "\t")
 
 
