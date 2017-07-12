fde906e946fb8ec3a3ffa925694617ce909277a5
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-12 09:54:11 -0500

    Fix

--- scripts/plot_coverage.R
+++ scripts/plot_coverage.R
@@ -15,10 +15,10 @@ all.data <- fread(inputFile)
 #   arrange(desc(C_ave))
   
 summ.data <- all.data %>%
-  mutate(C_weight = V4*(V3-V2+1)) %>%
+  mutate(L_len = V3-V2+1) %>%
+  mutate(C_weight = V4*L_len) %>%
   group_by(V1) %>%
-  #mutate(C_length = max(V3)) %>%
-  summarise(C_ave = mean(C_weight), C_length = max(V3)) %>%
+  summarise(C_ave = sum(C_weight)/sum(L_len), C_length = max(V3)) %>%
   arrange(desc(C_ave))
 
 
