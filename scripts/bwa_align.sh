81aeb7388545d696574e45f791f2027e9f6a7810
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-15 16:05:59 -0500

    Add zcat

--- scripts/bwa_align.sh
+++ scripts/bwa_align.sh
@@ -1,13 +1,15 @@
 # Project
 proj="Local_BgeVars"
 
-trimmomatic PE -phred33 -threads 4 ${GIT_DATA}/${proj}/CA301ANXX_1.fastq.gz ${GIT_DATA}/${proj}/CA301ANXX_2.fastq.gz ${GIT_DATA}/${proj}/CA301ANXX_P1.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_U1.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_P2.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_U2.fq.gz ILLUMINACLIP:${GIT_PATH}/${proj}/auxillary/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:15
+# trimmomatic PE -phred33 -threads 4 ${GIT_DATA}/${proj}/CA301ANXX_1.fastq.gz ${GIT_DATA}/${proj}/CA301ANXX_2.fastq.gz ${GIT_DATA}/${proj}/CA301ANXX_P1.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_U1.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_P2.fq.gz ${GIT_DATA}/${proj}/CA301ANXX_U2.fq.gz ILLUMINACLIP:${GIT_PATH}/${proj}/auxillary/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:15
 
 # wget -nc -O ${GIT_DATA}/${proj}/BglaB1.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02scaffoldsbglab1fagz
 # zcat ${GIT_DATA}/${proj}/BglaB1.fa.gz > ${GIT_DATA}/${proj}/BglaB1.fa
 
 # bwa index ${GIT_DATA}/${proj}/BglaB1.fa
-bwa mem -t 4 ${GIT_DATA}/${proj}/BglaB1.fa ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq.gz ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq.gz > ${GIT_DATA}/${proj}/CA301ANXX.sam
+zcat  ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq
+zcat ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq
+bwa mem -t 4 ${GIT_DATA}/${proj}/BglaB1.fa ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq > ${GIT_DATA}/${proj}/CA301ANXX.sam
 samtools view -bS ${GIT_DATA}/${proj}/CA301ANXX.sam > ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
 samtools flagstat ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
 samtools sort -@ 4 -o ${GIT_DATA}/${proj}/CA301ANXX.bam ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam 
