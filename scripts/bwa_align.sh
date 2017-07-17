f0505245b38f150517169f46d311eb2f1c4850b4
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-17 13:41:59 -0500

    Next step in bwa_align

--- scripts/bwa_align.sh
+++ scripts/bwa_align.sh
@@ -10,15 +10,15 @@ proj="Local_BgeVars"
 # zcat  ${GIT_DATA}/${proj}/CA301ANXX_P1.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq
 # zcat ${GIT_DATA}/${proj}/CA301ANXX_P2.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq
 # bwa mem -t 4 ${GIT_DATA}/${proj}/BglaB1.fa ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq > ${GIT_DATA}/${proj}/CA301ANXX.sam
-samtools view -@ 4 -bS ${GIT_DATA}/${proj}/CA301ANXX.sam > ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
+# samtools view -@ 4 -bS ${GIT_DATA}/${proj}/CA301ANXX.sam > ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
 # samtools flagstat ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
-samtools sort -@ 4 -m 2G -o ${GIT_DATA}/${proj}/CA301ANXX.bam ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam 
+# samtools sort -@ 4 -m 2G -o ${GIT_DATA}/${proj}/CA301ANXX.bam ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam 
 # rm ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
-zcat -c ${GIT_DATA}/${proj}/CA301ANXX.bam > ${GIT_DATA}/${proj}/CA301ANXX.bam.gz
-samtools index -@ 4 -b ${GIT_DATA}/${proj}/CA301ANXX.bam
+# zcat -c ${GIT_DATA}/${proj}/CA301ANXX.bam > ${GIT_DATA}/${proj}/CA301ANXX.bam.gz
+# samtools index -@ 4 -b ${GIT_DATA}/${proj}/CA301ANXX.bam
 	
 # samtools merge merge.bam ~/data/Bgla/BAM/*.bam 
 # bedtools genomecov -ibam ~/data/Bgla/BAM/merge.bam -d > ~/data/Bgla/BAM/merge.d.bedgraph
 # bedtools genomecov -ibam ~/data/Bgla/BAM/merge.bam -bga > ~/data/Bgla/BAM/merge.bga.bedgraph
-# bedtools genomecov -ibam ${GIT_DATA}/${proj}/CA301ANXX.bam -bga > ${GIT_DATA}/${proj}/CA301ANXX.bam
+bedtools genomecov -ibam ${GIT_DATA}/${proj}/CA301ANXX.bam -bga > ${GIT_DATA}/${proj}/CA301ANXX.bam
 # bedtools genomecov -ibam ~/data/Bge/GCF_000457365.1_ASM45736v1_genomic.sorted.bam -d > ~/data/Bgla/BAM/BGE.d.bedgraph
