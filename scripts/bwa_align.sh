12d1d1b0b891c3d8568aeb0b0697534e3529bbe7
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-07-17 07:27:31 -0500

    Finish alignment

--- scripts/bwa_align.sh
+++ scripts/bwa_align.sh
@@ -7,9 +7,9 @@ proj="Local_BgeVars"
 # zcat ${GIT_DATA}/${proj}/BglaB1.fa.gz > ${GIT_DATA}/${proj}/BglaB1.fa
 
 # bwa index ${GIT_DATA}/${proj}/BglaB1.fa
-zcat  ${GIT_DATA}/${proj}/CA301ANXX_P1.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq
-zcat ${GIT_DATA}/${proj}/CA301ANXX_P2.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq
-bwa mem -t 4 ${GIT_DATA}/${proj}/BglaB1.fa ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq > ${GIT_DATA}/${proj}/CA301ANXX.sam
+# zcat  ${GIT_DATA}/${proj}/CA301ANXX_P1.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq
+# zcat ${GIT_DATA}/${proj}/CA301ANXX_P2.fq.gz > ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq
+# bwa mem -t 4 ${GIT_DATA}/${proj}/BglaB1.fa ${GIT_DATA}/${proj}/CA301ANXX_P1.fastq ${GIT_DATA}/${proj}/CA301ANXX_P2.fastq > ${GIT_DATA}/${proj}/CA301ANXX.sam
 samtools view -bS ${GIT_DATA}/${proj}/CA301ANXX.sam > ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
 samtools flagstat ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam
 samtools sort -@ 4 -o ${GIT_DATA}/${proj}/CA301ANXX.bam ${GIT_DATA}/${proj}/CA301ANXX.unsorted.bam 
