2c9758c93b0f65ff5e24a1b206d0dec4cb8538e2
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-12-05 15:05:25 -0600

    Back to original

--- scripts/snpeff.sh
+++ scripts/snpeff.sh
@@ -17,45 +17,45 @@ mkdir -p ${genome_path}/${build}
 mkdir -p ${genome_path}/genomes
 
 # Update config file
-# echo "${build}.genome : B_glabrata" >> $genome_path/../snpEff.config
+echo "${build}.genome : B_glabrata" >> $genome_path/../snpEff.config
 
 # Download genome / Extract sequence 
-# wget -nc -O ${genome_path}/genomes/${build}.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02scaffoldsbglab1fagz
-# zcat ${genome_path}/genomes/${build}.fa.gz > ${genome_path}/genomes/${build}.fa
+wget -nc -O ${genome_path}/genomes/${build}.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02scaffoldsbglab1fagz
+zcat ${genome_path}/genomes/${build}.fa.gz > ${genome_path}/genomes/${build}.fa
 
 # Download/extract CDS
-# wget -nc -O ${genome_path}/${build}/cds.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02transcriptsbglab16fagz
-# zcat ${genome_path}/${build}/cds.fa.gz > ${genome_path}/${build}/cds.fa
+wget -nc -O ${genome_path}/${build}/cds.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02transcriptsbglab16fagz
+zcat ${genome_path}/${build}/cds.fa.gz > ${genome_path}/${build}/cds.fa
 
 # Download/extract proteins
-# wget -nc -O ${genome_path}/${build}/protein.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02peptidesbglab16fagz
-# zcat ${genome_path}/${build}/protein.fa.gz > ${genome_path}/${build}/protein.fa
+wget -nc -O ${genome_path}/${build}/protein.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02peptidesbglab16fagz
+zcat ${genome_path}/${build}/protein.fa.gz > ${genome_path}/${build}/protein.fa
 
 # Download/extract transcripts
-# wget -nc -O ${genome_path}/${build}/transcripts.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02transcriptsbglab16fagz
-# zcat ${genome_path}/${build}/transcripts.fa.gz > ${genome_path}/${build}/transcripts.fa
+wget -nc -O ${genome_path}/${build}/transcripts.fa.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02transcriptsbglab16fagz
+zcat ${genome_path}/${build}/transcripts.fa.gz > ${genome_path}/${build}/transcripts.fa
 
 # Download/extract gtf
-# wget -nc -O ${genome_path}/${build}/genes.gtf.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02basefeaturesbglab16gtfgz
-# zcat ${genome_path}/${build}/genes.gtf.gz > ${genome_path}/${build}/genes.gtf
+wget -nc -O ${genome_path}/${build}/genes.gtf.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02basefeaturesbglab16gtfgz
+zcat ${genome_path}/${build}/genes.gtf.gz > ${genome_path}/${build}/genes.gtf
 
 # Download/extract gff
 # wget -nc -O ${genome_path}/${build}/genes.gff.gz https://www.vectorbase.org/download/biomphalaria-glabrata-bb02basefeaturesbglab16gff3gz
 # zcat ${genome_path}/${build}/genes.gff.gz > ${genome_path}/${build}/genes.gff
 
 # correct GTF (removes transcript lines, start_codon, stop_codon, and UTRs)
-# cd ${genome_path}/${build}/
-# gffread -E genes.gtf -T -o genes2.gtf
+cd ${genome_path}/${build}/
+gffread -E genes.gtf -T -o genes2.gtf
 # gffread -V -H  -N -J genes.gtf -T -o genes2.gtf -g ../genomes/BglaB1.6.fa
-# mv genes2.gtf genes.gtf
+mv genes2.gtf genes.gtf
 
 # Build genome 
-# cd ${genome_path}
-# snpEff build -gtf22 -v ${build}
+cd ${genome_path}
+snpEff build -gtf22 -v ${build}
 # snpEff build -gff3 -v ${build}
 
 # run snpEff
-# cp ~/GHdata/BgeVars/Bge/bcftools.snp.fil3.vcf ${genome_path}/${build}/
+cp ~/GHdata/BgeVars/Bge/bcftools.snp.fil3.vcf ${genome_path}/${build}/
 cd ${genome_path}/${build}
 snpEff -c ../../snpEff.config -v BglaB1.6 bcftools.snp.fil3.vcf > bcftools.snp.fil3.ann.vcf
 
